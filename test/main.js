66a3ca61b26ac851c5dd335bf47c77dd9c9c0260
scottmotte <scott@scottmotte.com>
2013-07-06 00:53:44 +0000

    Support quotes

--- test/main.js
+++ test/main.js
@@ -2,14 +2,25 @@ var assert      = require('assert'),
     should      = require('should'),
     dotenv      = require('../lib/main');
 
+var result;
+
 describe('dotenv', function() {
   describe('.load()', function() {
-    it('sets the environment variables', function() {
-      var result = dotenv();
-
+    before(function() {
+      result = dotenv();
       result.load();
-      
+    });
+
+    it('sets the basic environment variables', function() {
       process.env.BASIC.should.eql("basic");
     });
+
+    it('sets double quotes environment variables', function() {
+      process.env.DOUBLE_QUOTES.should.eql("double_quotes");
+    });
+    it('sets single quotes environment variables', function() {
+      process.env.SINGLE_QUOTES.should.eql("single_quotes");
+    });
+
   });
 });
