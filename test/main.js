5bb99e857d6646b85142f5c6aedaf5f6a879bfa6
scottmotte <scott@scottmotte.com>
2013-10-30 18:56:01 -0700

    Remove any empty lines in the .env* files

--- test/main.js
+++ test/main.js
@@ -25,13 +25,23 @@ describe('dotenv', function() {
     it('sets double quotes environment variables', function() {
       process.env.DOUBLE_QUOTES.should.eql("double_quotes");
     });
+
     it('sets single quotes environment variables', function() {
       process.env.SINGLE_QUOTES.should.eql("single_quotes");
     });
+
     it('expands newlines but only if double quoted', function() {
       process.env.EXPAND_NEWLINES.should.eql("expand\nnewlines");
       process.env.DONT_EXPAND_NEWLINES_1.should.eql("dontexpand\\nnewlines");
       process.env.DONT_EXPAND_NEWLINES_2.should.eql("dontexpand\\nnewlines");
+    });
+
+    it('reads from .env.development', function() {
+      process.env.FROM_DEVELOPMENT_ENV.should.eql("from_development_env");
+    });
+
+    it('reads from a skipped line in .env.development', function() {
+      process.env.AFTER_LINE.should.eql("after_line");
     });
   });
 });
