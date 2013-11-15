16f8f07fc1e92807ea369a99da9194378b544a19
scottmotte <scott@scottmotte.com>
2013-11-14 17:13:06 -0800

    Version bump 0.2.1

--- test/main.js
+++ test/main.js
@@ -10,7 +10,7 @@ describe('dotenv', function() {
   });
 
   it('version should be set', function() {
-    result.version.should.eql("0.2.0"); 
+    result.version.should.eql("0.2.1"); 
   });
 
   describe('.load()', function() {
