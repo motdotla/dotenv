86669004bf19b6c79ab76372e45963e05640024d
Roman Fromrome <romanfromrome@gmail.com>
2014-12-28 15:37:59 +0200

    refactoring and load() method return values check for successfulness

--- test/main.js
+++ test/main.js
@@ -12,7 +12,7 @@ describe('dotenv', function() {
   });
 
   it('version should be set', function() {
-    result.version.should.eql("0.4.0"); 
+    result.version.should.eql("0.4.0");
   });
 
   describe('.load()', function() {
@@ -80,7 +80,7 @@ describe('dotenv', function() {
     it('reads from a skipped line in .env.development', function() {
       process.env.AFTER_LINE.should.eql("after_line");
     });
-    
+
     it('ignores commented lines', function() {
       should.not.exist(process.env.COMMENTS);
     });
@@ -92,13 +92,21 @@ describe('dotenv', function() {
     it('should handle zero width unicode characters', function() {
       process.env.ZERO_WIDTH_CHARACTER.should.eql("user:pass@troup.mongohq.com:1004/dude");
     });
-    
+
     it ('retains inner quotes', function() {
       process.env.RETAIN_INNER_QUOTES.should.eql('{"foo": "bar"}');
       process.env.RETAIN_INNER_QUOTES_AS_STRING.should.eql('{"foo": "bar"}');
     });
 
+    it ('returns false if file does not exists', function() {
+      process.env.NODE_ENV = 'fakeenvname';
+      result.load().should.eql(false);
+      delete process.env.NODE_ENV;
+    });
 
+    it ('returns true if file does exists', function() {
+      result.load().should.eql(true);
+    });
   });
 
   describe('.load() after an ENV was already set on the machine', function() {
