8252786cebf934d0282311f945b9d948000f3353
Max Beatty <max@isocket.com>
2015-09-10 12:17:07 -0700

    add examples for how to use module in different ways

---
+++ examples/preload/index.js
@@ -0,0 +1,4 @@
+// dotenv invoked via preload command line argument
+// e.g. node -r dotenv/config index.js
+// run this example by executing the "run_me" script: ./run_me
+console.log(process.env.PRELOAD)
