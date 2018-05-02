c74272f608a0185c8e1e6ff6571b35380ce55dd5
Max Beatty <max.beatty@ansaro.ai>
2018-05-01 23:56:21 -0700

    try sync child for windows tests

--- tests/test-config-cli.js
+++ tests/test-config-cli.js
@@ -1,18 +1,22 @@
 const cp = require('child_process')
+const path = require('path')
 
 const test = require('tap').test
 
 const nodeBinary = process.argv[0]
 
 test('config preload loads .env', t => {
-  t.plan(2)
+  t.plan(1)
+
   // NB: `nodeBinary` is quoted for Windows
-  cp.exec(
-    '"' + nodeBinary + '" -r ../config -e "console.log(process.env.BASIC)" dotenv_config_path=./tests/.env',
-    function (err, stdout) {
-      t.error(err)
-      t.equal(stdout.trim(), 'basic')
-      t.end() // explicit for Windows
+  const stdout = cp.execSync(
+    '"' + nodeBinary + '" -r ../config -e "console.log(process.env.BASIC)" dotenv_config_encoding=utf8',
+    {
+      cwd: path.resolve(__dirname),
+      timeout: 500,
+      encoding: 'utf8'
     }
   )
+
+  t.equal(stdout.trim(), 'basic')
 })
