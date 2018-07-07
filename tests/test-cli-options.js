907fcc6e90faacbd1a8665134b8004a1c5475d18
Max Beatty <max.beatty@ansaro.ai>
2018-07-07 08:11:15 -0700

    ignore unsupported cli options ref #313

--- tests/test-cli-options.js
+++ tests/test-cli-options.js
@@ -2,12 +2,20 @@ const t = require('tap')
 
 const options = require('../lib/cli-options')
 
-t.plan(2)
+t.plan(4)
 
+// matches encoding option
 t.same(options(['node', '-e', "'console.log(testing)'", 'dotenv_config_encoding=utf8']), {
   encoding: 'utf8'
 })
 
+// matches path option
 t.same(options(['node', '-e', "'console.log(testing)'", 'dotenv_config_path=/custom/path/to/your/env/vars']), {
   path: '/custom/path/to/your/env/vars'
 })
+
+// ignores empty values
+t.same(options(['node', '-e', "'console.log(testing)'", 'dotenv_config_path=']), {})
+
+// ignores unsupported options
+t.same(options(['node', '-e', "'console.log(testing)'", 'dotenv_config_foo=bar']), {})
