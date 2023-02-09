428c94ababa7fc81d5b7d7adb0b1c235fea3f29f
Samarth Kulkarni <10831629+ParadoxInfinite@users.noreply.github.com>
2023-02-09 13:05:09 +0530

    fix: passing test cases, coverage not 100% yet

--- tests/test-safe.js
+++ tests/test-safe.js
@@ -4,13 +4,16 @@ const sinon = require('sinon')
 const t = require('tap')
 
 const dotenv = require('../lib/main')
+const MissingEnvVarsError = require('../lib/missing-vars-error')
 
 const mockParseResponse = { test: 'foo' }
 let readFileSyncStub
 let parseStub
 
 t.beforeEach(() => {
-  readFileSyncStub = sinon.stub(fs, 'readFileSync').returns('test=foo')
+  readFileSyncStub = sinon.stub(fs, 'readFileSync').onFirstCall().returns('test=foo')
+  readFileSyncStub.onSecondCall().returns('test=foo')
+  readFileSyncStub.onThirdCall().returns('nonExistentKey=foo')
   parseStub = sinon.stub(dotenv, 'parse').returns(mockParseResponse)
 })
 
@@ -22,18 +25,16 @@ t.afterEach(() => {
 t.test('takes option for example', ct => {
   ct.plan(1)
 
-  const testPath = 'tests/.env.example'
-  dotenv.safe({ example: testPath })
-
-  ct.equal(readFileSyncStub.args[0][0], testPath)
+  const examplePath = 'tests/.env.example'
+  dotenv.safe({ example: examplePath })
+  ct.equal(readFileSyncStub.args[1][0], examplePath)
 })
 
-t.test('takes option for allowEmptyValues', ct => {
+t.test('returns any errors thrown from missing env', ct => {
   ct.plan(1)
 
-  const logStub = sinon.stub(console, 'log')
-  dotenv.config({ allowEmptyValues: true })
+  const examplePath = 'tests/.env.example'
+  dotenv.safe({ example: examplePath })
 
-  ct.ok(logStub.called)
-  logStub.restore()
+  ct.throws(function () { throw new MissingEnvVarsError() }, {})
 })
