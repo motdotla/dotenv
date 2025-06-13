766fff6e3fe5a857354ea00a64419e08fc742054
Shyok Mutsuddi <shyokmutsuddi21@gmail.com>
2025-06-13 14:21:00 +0000

    test: add fixture setup for nearest strategy tests

---
+++ tests/fixtures/strategy-nearest/load-env-default-strategy.js
@@ -0,0 +1,4 @@
+require('../../../lib/main').config()
+module.exports = function getEnv () {
+  return process.env.TEST_KEY
+}
---
+++ tests/fixtures/strategy-nearest/load-env-nearest-strategy.js
@@ -0,0 +1,4 @@
+require('../../../lib/main').config({ strategy: 'nearest' })
+module.exports = function getEnv () {
+  return process.env.TEST_KEY
+}
